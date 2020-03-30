176e5aebbb50716867e3005d229c8a499039fd6a
railander <railanderguilherme@gmail.com>
2020-03-30 13:10:55 -0300

    separação repositorios

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './pages/global.css';
+
+import Routes from './routes';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <Routes/>
+    </React.Fragment>
+  );
+}
+
+export default App;
